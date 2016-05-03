5bbf35550b94227a01c005e88f3e66030b930c9f
nath@ulb <nathdwek@ukb.ac.be>
2016-05-03 17:30:21 +0200

    rewrite gardner

--- gardner.m
+++ gardner.m
@@ -1,19 +1,18 @@
-function [corrected] = gardner(sampled, K)
-    global TGARDNER FM;
-    epsilon = zeros(ceil(length(sampled)/2), 1);
+function corrected = gardner(sampled, K)
+    global TGARDNER FSGARDNER FM;
+    osratio = FSGARDNER/FM
+    epsilon = zeros(ceil(size(sampled)./osratio));
     corrected = zeros(length(epsilon), 1);
     corrected(1) = sampled(1);
     for n = 1:length(epsilon)-1
-        interpolation = interp1(1:3,sampled(2*n-1:2*n+1), (1:3)+epsilon(n), 'cubic');
-        corrected(n) = interpolation(1);
-        middle = interpolation(2);
-        corrected(n+1) = interpolation(3);
-        epsilon(n+1) = epsilon(n) + 2*K/FM*real(middle*(conj(corrected(n+1)) - conj(corrected(n))));
+        interpolation = interp1(1:osratio+1,sampled(osratio*(n-1)+1:osratio*n+1), [osratio/2+1 osratio+1] + epsilon(n), 'pchip');
+        middle = interpolation(1);
+        corrected(n+1) = interpolation(2);
+        epsilon(n+1) = epsilon(n) - 2*K/FM*real(middle*(conj(corrected(n+1)) - conj(corrected(n))));
     end
     if TGARDNER
         figure;
         plot(epsilon);
-        length(epsilon)
         title('epsilon')
     end
 end
